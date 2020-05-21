09286d9cd79ac584cdf0094620d853ea0e134e38
German Tellez <gertellezv@gmail.com>
2020-05-21 10:36:03 -0500

    prod

--- daras/settings.py
+++ daras/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'x2a$32hddzc-f7+5(bk7a*ijk+y4$cdt+5^_q@n3ojgvcc^o=z'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
