5f5b61db92090a192b9e3b8dae70123633f5345c
German Tellez <gertellezv@gmail.com>
2020-05-22 12:00:41 -0500

    statics

--- daras/settings.py
+++ daras/settings.py
@@ -128,17 +128,17 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 
-STATIC_ROOT = os.path.join(BASE_DIR,'../staticfiles/')
+STATIC_ROOT = os.path.join(BASE_DIR,'/staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, '/static'),
 ]
 
-# STATICFILES_FINDERS = [
-#     'django.contrib.staticfiles.finders.FileSystemFinder',
-#     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-# ]
+STATICFILES_FINDERS = [
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+]
 
 # MEDIA SETTINGS
 MEDIA_URL = '/media/'
